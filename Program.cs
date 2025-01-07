a62c6db393db0272a8636452ccd1562ff88a7ad9
Tanvir <santoahmed225@gmail.com>
2025-01-07 10:30:47 +0600

    factorial of any number done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using System.Net.WebSockets;
+using System.Numerics;
+class Test{
+
+    public static void Main(string[] args){
+
+        Console.WriteLine("Enter your number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int fact = 1;
+        for (int i = 1; i <= num ; i++){
+            fact = fact * i;
+        }
+        Console.WriteLine($"Factorial of({num})= {fact}");
+
+
+    }
+
+
+
+
+
+}
